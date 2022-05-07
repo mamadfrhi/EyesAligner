320d039b915ee05596ec3fa392558d636039f283
Mamad Farrahi <mamad.frhi@gmail.com>
2022-05-07 20:41:56 +0200

    Initial Commit

---
+++ EyesAligner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EyesAligner
+//
+//  Created by iMamad on 07.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
