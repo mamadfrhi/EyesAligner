7ceaa6603b1c06f29d6209eb212b4cda2ff6156e
Mamad Farrahi <mamad.frhi@gmail.com>
2022-05-08 08:41:22 +0200

    added startup of the app in AppDelegate.

--- EyesAligner/AppDelegate.swift
+++ EyesAligner/AppDelegate.swift
@@ -9,6 +9,15 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
     
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        let mainVM = MainVM()
+        window?.rootViewController = MainVC.`init`(mainVM: mainVM)
+        
+        return true
+    }
 }
 
