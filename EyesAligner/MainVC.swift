71689da07343032a64dc67254278bba06fd869cb
Mamad Farrahi <mamad.frhi@gmail.com>
2022-05-08 08:29:33 +0200

    implemented base architecture of the app.

---
+++ EyesAligner/MainVC.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  EyesAligner
+//
+//  Created by iMamad on 07.05.22.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+extension MainVC: MainVMViewDelegate {
+    func handleFaceDetectionResults(_ observedFaces: [VNFaceObservation]) {
+        
+        self.clearDrawings()
+        let facesBoundingBoxes: [CAShapeLayer] = observedFaces.flatMap({ (observedFace: VNFaceObservation) -> [CAShapeLayer] in
+            let faceBoundingBoxOnScreen = self.previewLayer.layerRectConverted(fromMetadataOutputRect: observedFace.boundingBox)
+            let faceBoundingBoxPath = CGPath(rect: faceBoundingBoxOnScreen, transform: nil)
+            let faceBoundingBoxShape = CAShapeLayer()
+            faceBoundingBoxShape.path = faceBoundingBoxPath
+            faceBoundingBoxShape.fillColor = UIColor.clear.cgColor
+            faceBoundingBoxShape.strokeColor = UIColor.green.cgColor
+            var newDrawings = [CAShapeLayer]()
+            newDrawings.append(faceBoundingBoxShape)
+            if let landmarks = observedFace.landmarks {
+                newDrawings = newDrawings + self.drawFaceFeatures(landmarks, screenBoundingBox: faceBoundingBoxOnScreen)
+            }
+            return newDrawings
+        })
+        facesBoundingBoxes.forEach({ faceBoundingBox in self.view.layer.addSublayer(faceBoundingBox) })
+        self.drawings = facesBoundingBoxes
+    }
+    
+    func clearDrawings() {
+        self.drawings.forEach({ drawing in drawing.removeFromSuperlayer() })
+    }
+}
+class MainVC: UIViewController {
+    
+    var viewModel: MainVM! {
+        didSet {
+            viewModel.viewDelegate = self
+        }
+    }
+    
+    private lazy var previewLayer = AVCaptureVideoPreviewLayer(session: viewModel.captureSession)
+    
+    private var drawings: [CAShapeLayer] = []
+    
+    private var mainVM: MainVM? {
+        didSet {
+            mainVM!.viewDelegate = self
+        }
+    }
+
+    // MARK: - VC
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.previewLayer.frame = self.view.frame
+    }
+    
+    // MARK: - Config
+    // 2- fill previewLayer
+    func configPreviewLayer() {
+        self.previewLayer.videoGravity = .resizeAspectFill
+        self.view.layer.addSublayer(self.previewLayer)
+        self.previewLayer.frame = self.view.frame
+    }
+}
+
+
+// drawing
+extension MainVC {
+    private func drawFaceFeatures(_ landmarks: VNFaceLandmarks2D, screenBoundingBox: CGRect) -> [CAShapeLayer] {
+        var faceFeaturesDrawings: [CAShapeLayer] = []
+        if let leftEye = landmarks.leftEye {
+            let eyeDrawing = self.drawEye(leftEye, screenBoundingBox: screenBoundingBox)
+            faceFeaturesDrawings.append(eyeDrawing)
+        }
+        if let rightEye = landmarks.rightEye {
+            let eyeDrawing = self.drawEye(rightEye, screenBoundingBox: screenBoundingBox)
+            faceFeaturesDrawings.append(eyeDrawing)
+        }
+        // draw other face features here
+        return faceFeaturesDrawings
+    }
+    private func drawEye(_ eye: VNFaceLandmarkRegion2D, screenBoundingBox: CGRect) -> CAShapeLayer {
+        let eyePath = CGMutablePath()
+        let eyePathPoints = eye.normalizedPoints
+            .map({ eyePoint in
+                CGPoint(
+                    x: eyePoint.y * screenBoundingBox.height + screenBoundingBox.origin.x,
+                    y: eyePoint.x * screenBoundingBox.width + screenBoundingBox.origin.y)
+            })
+        eyePath.addLines(between: eyePathPoints)
+        eyePath.closeSubpath()
+        let eyeDrawing = CAShapeLayer()
+        eyeDrawing.path = eyePath
+        eyeDrawing.fillColor = UIColor.clear.cgColor
+        eyeDrawing.strokeColor = UIColor.green.cgColor
+        
+        return eyeDrawing
+    }
+}
+
+
---
+++ EyesAligner/MainVM.swift
@@ -0,0 +1,82 @@
+//
+//  MainVM.swift
+//  EyesAligner
+//
+//  Created by iMamad on 08.05.22.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+protocol MainVMViewDelegate {
+    func handleFaceDetectionResults(_ observedFaces: [VNFaceObservation])
+    func clearDrawings()
+    func configPreviewLayer()
+}
+class MainVM: NSObject {
+    var viewDelegate: MainVMViewDelegate?
+    
+    func start() {
+        self.addCameraInput()
+        viewDelegate?.configPreviewLayer()
+        self.getCameraFrames()
+        self.captureSession.startRunning()
+    }
+    
+    let captureSession = AVCaptureSession() //it's front camera within itself
+    // 1- fill capture session from device
+    private func addCameraInput() {
+        guard let device = AVCaptureDevice.DiscoverySession(
+            deviceTypes: [.builtInWideAngleCamera, .builtInDualCamera, .builtInTrueDepthCamera],
+            mediaType: .video,
+            position: .front).devices.first else {
+                fatalError("No back camera device found, please make sure to run SimpleLaneDetection in an iOS device and not a simulator")
+        }
+        let cameraInput = try! AVCaptureDeviceInput(device: device)
+        self.captureSession.addInput(cameraInput)
+    }
+    
+    private let videoDataOutput = AVCaptureVideoDataOutput()
+    // 3- config videoDataOutput and add it on captureSession
+    private func getCameraFrames() {
+        self.videoDataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String : Any]
+        self.videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        self.videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
+        self.captureSession.addOutput(self.videoDataOutput)
+        guard let connection = self.videoDataOutput.connection(with: AVMediaType.video),
+            connection.isVideoOrientationSupported else { return }
+        connection.videoOrientation = .portrait
+    }
+    
+    // 5- perform face detection command
+    private func detectFace(in image: CVPixelBuffer) {
+        let faceDetectionRequest = VNDetectFaceLandmarksRequest(completionHandler: { (request: VNRequest, error: Error?) in
+            DispatchQueue.main.async {
+                if let results = request.results as? [VNFaceObservation] {
+                    self.viewDelegate?.handleFaceDetectionResults(results)
+                } else {
+                    self.viewDelegate?.clearDrawings()
+                }
+            }
+        })
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: image, orientation: .leftMirrored, options: [:])
+        try? imageRequestHandler.perform([faceDetectionRequest])
+    }
+}
+
+extension MainVM: AVCaptureVideoDataOutputSampleBufferDelegate {
+    // 4- convert output to frame
+    // and call detectFace
+    func captureOutput(
+        _ output: AVCaptureOutput,
+        didOutput sampleBuffer: CMSampleBuffer,
+        from connection: AVCaptureConnection) {
+        
+        guard let frame = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            debugPrint("unable to get image from sample buffer")
+            return
+        }
+        self.detectFace(in: frame)
+    }
+}
