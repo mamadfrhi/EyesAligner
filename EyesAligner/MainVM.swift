b8f8f0cc5e93391957e9f288cbb0cff3d42704e2
Mamad Farrahi <mamad.frhi@gmail.com>
2022-05-08 16:44:44 +0200

    cleaned MainVM file.

--- EyesAligner/MainVM.swift
+++ EyesAligner/MainVM.swift
@@ -16,8 +16,16 @@ protocol MainVMViewDelegate {
     func updateLabel(text: String)
 }
 class MainVM: NSObject {
+    
+    // MARK: Properties
     var viewDelegate: MainVMViewDelegate?
     
+    private let videoDataOutput = AVCaptureVideoDataOutput()
+    
+    private let captureSession = AVCaptureSession()
+    
+    
+    // MARK: Funcs
     func start() {
         self.addCameraInput()
         viewDelegate!.configPreviewLayer()
@@ -31,11 +39,11 @@ class MainVM: NSObject {
         
         if let leftEye = face.leftEye, let rightEye = face.rightEye {
             let leftEyePoint = getTransformedPoints(landmark: leftEye,
-                                                        faceRect: face.faceRect,
-                                                        imageSize: imageSize)
+                                                    faceRect: face.faceRect,
+                                                    imageSize: imageSize)
             let rightEyePoint = getTransformedPoints(landmark: rightEye,
-                                                        faceRect: face.faceRect,
-                                                        imageSize: imageSize)
+                                                     faceRect: face.faceRect,
+                                                     imageSize: imageSize)
             let isInGoldenArea = goldenArea.contains(leftEyePoint.first!) && goldenArea.contains(rightEyePoint.last!)
             if isInGoldenArea {
                 viewDelegate?.updateLabel(text: "Good ✅")
@@ -45,45 +53,40 @@ class MainVM: NSObject {
         }
     }
     
-    func getTransformedPoints(
+    private func getTransformedPoints(
         landmark:VNFaceLandmarkRegion2D,
         faceRect:CGRect,
         imageSize:CGSize) -> [CGPoint]{
-            
-            // last point is 0.0
-            return landmark.normalizedPoints.map({ (np) -> CGPoint in
+            return landmark.normalizedPoints.map{ (np) -> CGPoint in
                 return CGPoint(
                     x: faceRect.origin.x + np.x * faceRect.size.width,
                     y: imageSize.height - (np.y * faceRect.size.height + faceRect.origin.y))
-            })
+            }
         }
     
-    let captureSession = AVCaptureSession() //it's front camera within itself
-    // 1- fill capture session from device
+    
+    
     private func addCameraInput() {
         guard let device = AVCaptureDevice.DiscoverySession(
             deviceTypes: [.builtInWideAngleCamera, .builtInDualCamera, .builtInTrueDepthCamera],
             mediaType: .video,
             position: .front).devices.first else {
-                fatalError("No back camera device found, please make sure to run SimpleLaneDetection in an iOS device and not a simulator")
+            fatalError("No back camera device found, please make sure to run SimpleLaneDetection in an iOS device and not a simulator")
         }
         let cameraInput = try! AVCaptureDeviceInput(device: device)
         self.captureSession.addInput(cameraInput)
     }
     
-    private let videoDataOutput = AVCaptureVideoDataOutput()
-    // 3- config videoDataOutput and add it on captureSession
     private func getCameraFrames() {
         self.videoDataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String : Any]
         self.videoDataOutput.alwaysDiscardsLateVideoFrames = true
         self.videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
         self.captureSession.addOutput(self.videoDataOutput)
         guard let connection = self.videoDataOutput.connection(with: AVMediaType.video),
-            connection.isVideoOrientationSupported else { return }
+              connection.isVideoOrientationSupported else { return }
         connection.videoOrientation = .portrait
     }
     
-    // 5- perform face detection command
     private func detectFace(in image: CVPixelBuffer) {
         let faceDetectionRequest = VNDetectFaceLandmarksRequest(completionHandler: { (request: VNRequest, error: Error?) in
             if let faces = request.results as? [VNFaceObservation], let firstFace = faces.first {
@@ -98,8 +101,6 @@ class MainVM: NSObject {
 }
 
 extension MainVM: AVCaptureVideoDataOutputSampleBufferDelegate {
-    // 4- convert output to frame
-    // and call detectFace
     func captureOutput(
         _ output: AVCaptureOutput,
         didOutput sampleBuffer: CMSampleBuffer,
