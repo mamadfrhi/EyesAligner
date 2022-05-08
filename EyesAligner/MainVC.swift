dad29bec666f2826fab62baa07c4db08107d901b
Mamad Farrahi <mamad.frhi@gmail.com>
2022-05-08 09:28:42 +0200

    cleaned MainVC file.

--- EyesAligner/MainVC.swift
+++ EyesAligner/MainVC.swift
@@ -9,39 +9,7 @@ import UIKit
 import AVFoundation
 import Vision
 
-extension MainVC: MainVMViewDelegate {
-    func handleFaceDetectionResults(_ observedFaces: [VNFaceObservation]) {
-        
-        self.clearDrawings()
-        let facesBoundingBoxes: [CAShapeLayer] = observedFaces.flatMap({ (observedFace: VNFaceObservation) -> [CAShapeLayer] in
-            let faceBoundingBoxOnScreen = self.previewLayer.layerRectConverted(fromMetadataOutputRect: observedFace.boundingBox)
-            let faceBoundingBoxPath = CGPath(rect: faceBoundingBoxOnScreen, transform: nil)
-            let faceBoundingBoxShape = CAShapeLayer()
-            faceBoundingBoxShape.path = faceBoundingBoxPath
-            faceBoundingBoxShape.fillColor = UIColor.clear.cgColor
-            faceBoundingBoxShape.strokeColor = UIColor.green.cgColor
-            var newDrawings = [CAShapeLayer]()
-            newDrawings.append(faceBoundingBoxShape)
-            if let landmarks = observedFace.landmarks {
-                newDrawings = newDrawings + self.drawFaceFeatures(landmarks, screenBoundingBox: faceBoundingBoxOnScreen)
-            }
-            return newDrawings
-        })
-        facesBoundingBoxes.forEach({ faceBoundingBox in self.view.layer.addSublayer(faceBoundingBox) })
-        self.drawings = facesBoundingBoxes
-    }
-    
-    func configPreviewLayer() {
-        self.previewLayer.videoGravity = .resizeAspectFill
-        self.view.layer.addSublayer(self.previewLayer)
-        self.previewLayer.frame = self.view.frame
-    }
-    
-    func clearDrawings() {
-        self.drawings.forEach({ drawing in drawing.removeFromSuperlayer() })
-    }
 
-}
 class MainVC: UIViewController {
     
     class func `init`(mainVM: MainVM) -> MainVC {
@@ -50,6 +18,7 @@ class MainVC: UIViewController {
         return vc
     }
     
+    // MARK: Properties
     var mainVM: MainVM! {
         didSet {
             mainVM!.viewDelegate = self
@@ -60,9 +29,7 @@ class MainVC: UIViewController {
     
     private var drawings: [CAShapeLayer] = []
     
-    
-
-    // MARK: - VC
+    // MARK: Functions
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         self.previewLayer.frame = self.view.frame
@@ -70,7 +37,7 @@ class MainVC: UIViewController {
 }
 
 
-// drawing
+// MARK: - Drawings
 extension MainVC {
     private func drawFaceFeatures(_ landmarks: VNFaceLandmarks2D, screenBoundingBox: CGRect) -> [CAShapeLayer] {
         var faceFeaturesDrawings: [CAShapeLayer] = []
@@ -105,3 +72,36 @@ extension MainVC {
 }
 
 
+// MARK: - ViewDelegate
+extension MainVC: MainVMViewDelegate {
+    func handleFaceDetectionResults(_ observedFaces: [VNFaceObservation]) {
+        
+        self.clearDrawings()
+        let facesBoundingBoxes: [CAShapeLayer] = observedFaces.flatMap({ (observedFace: VNFaceObservation) -> [CAShapeLayer] in
+            let faceBoundingBoxOnScreen = self.previewLayer.layerRectConverted(fromMetadataOutputRect: observedFace.boundingBox)
+            let faceBoundingBoxPath = CGPath(rect: faceBoundingBoxOnScreen, transform: nil)
+            let faceBoundingBoxShape = CAShapeLayer()
+            faceBoundingBoxShape.path = faceBoundingBoxPath
+            faceBoundingBoxShape.fillColor = UIColor.clear.cgColor
+            faceBoundingBoxShape.strokeColor = UIColor.green.cgColor
+            var newDrawings = [CAShapeLayer]()
+            newDrawings.append(faceBoundingBoxShape)
+            if let landmarks = observedFace.landmarks {
+                newDrawings = newDrawings + self.drawFaceFeatures(landmarks, screenBoundingBox: faceBoundingBoxOnScreen)
+            }
+            return newDrawings
+        })
+        facesBoundingBoxes.forEach({ faceBoundingBox in self.view.layer.addSublayer(faceBoundingBox) })
+        self.drawings = facesBoundingBoxes
+    }
+    
+    func configPreviewLayer() {
+        self.previewLayer.videoGravity = .resizeAspectFill
+        self.view.layer.addSublayer(self.previewLayer)
+        self.previewLayer.frame = self.view.frame
+    }
+    
+    func clearDrawings() {
+        self.drawings.forEach({ drawing in drawing.removeFromSuperlayer() })
+    }
+}
