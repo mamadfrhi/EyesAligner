0132206b865535bbe7f327f71dad0699ac8b6510
Mamad Farrahi <mamad.frhi@gmail.com>
2022-05-08 08:50:54 +0200

    improved AppDelegate codes.

--- EyesAligner/AppDelegate.swift
+++ EyesAligner/AppDelegate.swift
@@ -13,9 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window = UIWindow()
+        window = UIWindow(frame: UIScreen.main.bounds)
         let mainVM = MainVM()
-        window?.rootViewController = MainVC.`init`(mainVM: mainVM)
+        window!.rootViewController = MainVC.`init`(mainVM: mainVM)
         
         return true
     }
